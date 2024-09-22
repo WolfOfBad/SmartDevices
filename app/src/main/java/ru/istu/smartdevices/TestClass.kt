edc5676751f68848228e5e30d2c893633b3066d4
WolfOfBad <mechkov.igor04@gmail.com>
2024-09-22 16:18:39 +0400

    prepare repo

---
+++ app/src/main/java/ru/istu/smartdevices/TestClass.kt
@@ -0,0 +1,9 @@
+package ru.istu.smartdevices
+
+class TestClass {
+    fun first(): Int = 1
+
+    fun second(): Int = 2
+
+    fun third(): Int = 3
+}
---
+++ app/src/test/java/ru/istu/smartdevices/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+package ru.istu.smartdevices
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertThat(2 + 2).isEqualTo(4)
+    }
+
+    @Test
+    fun t1() {
+        val obj = TestClass()
+
+        assertThat(obj.first()).isEqualTo(1)
+        assertThat(obj.second()).isEqualTo(2)
+    }
+}
