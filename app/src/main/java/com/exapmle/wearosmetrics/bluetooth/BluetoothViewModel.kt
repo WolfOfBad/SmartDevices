5dec61a6ecd57c44a952f2d64f8be72d46d361e0
Legion <khokhryakov2004@gmail.com>
2024-11-22 21:58:41 +0400

    made connection w/ mock +"connected"

--- app/src/main/java/com/exapmle/wearosmetrics/bluetooth/BluetoothViewModel.kt
+++ app/src/main/java/com/exapmle/wearosmetrics/bluetooth/BluetoothViewModel.kt
@@ -2,6 +2,7 @@ package ru.istu.smartdevices.bluetooth
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.wearosmetrics.bluetooth.BluetoothServiceInterface
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -10,7 +11,7 @@ import javax.inject.Inject
 class BluetoothViewModel
     @Inject
     constructor(
-        private val bluetoothService: BluetoothService,
+        private val bluetoothService: BluetoothServiceInterface,
     ) : ViewModel() {
         val connectionState = bluetoothService.connectionState
         val metrics = bluetoothService.metrics
